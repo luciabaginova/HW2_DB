4c6fa1e5d680798abe4bb3958646a1b339e3102d
luciabaginova <540495@muni.cz>
2024-05-24 19:56:08 +0200

    Update queries.js

--- queries.js
+++ queries.js
@@ -6,10 +6,10 @@ const pool = new Pool({
   host: `${process.env.DB_HOST}`,
   database: `${process.env.DB_DATABASE}`,
   password: `${process.env.DB_PASSWORD}`,
-    port: `${process.env.DB_PORT}`,
-    ssl: {
-        rejectUnauthorized: false
-    }
+  port: `${process.env.DB_PORT}`,
+  ssl: {
+      rejectUnauthorized: false
+  }
 })
 
 // Our first endpoint will be a GET request. 
